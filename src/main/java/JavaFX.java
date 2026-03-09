dadcdd174c0c51f2fb15993cf954668381c92760
AlgoriThai07 <thaiviet0703@gmail.com>
2026-03-08 23:17:05 -0500

    Organize project structure

--- src/main/java/JavaFX.java
+++ src/main/java/JavaFX.java
@@ -1,6 +1,8 @@
 import hourlyWeather.HourlyPeriod;
 import javafx.application.Application;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.scene.control.TextField;
@@ -22,29 +24,34 @@ public class JavaFX extends Application {
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("I'm a professional Weather App!");
-		ArrayList<HourlyPeriod> forecast = MyWeatherAPI.getHourlyForecast("LOT",77,70);
-		PointData pointData = MyWeatherAPI.getPointData(47.62350, -122.33290);
-		if (forecast == null){
-			throw new RuntimeException("Forecast did not load");
-		}
-		temperature = new TextField();
-		weather = new TextField();
-		temperature.setText("Today's weather is: "+String.valueOf(forecast.get(0).temperature));
-		weather.setText(forecast.get(0).shortForecast);
-		if (pointData == null){
-			throw new RuntimeException("Point Data did not load");
-		}
-		gridX = new TextField();
-		gridY = new TextField();
-		gridX.setText(String.valueOf(pointData.gridX));
-		gridY.setText(String.valueOf(pointData.gridY));
+//		primaryStage.setTitle("I'm a professional Weather App!");
+//		ArrayList<HourlyPeriod> forecast = MyWeatherAPI.getHourlyForecast("LOT",77,70);
+//		PointData pointData = MyWeatherAPI.getPointData(47.62350, -122.33290);
+//		if (forecast == null){
+//			throw new RuntimeException("Forecast did not load");
+//		}
+//		temperature = new TextField();
+//		weather = new TextField();
+//		temperature.setText("Today's weather is: "+String.valueOf(forecast.get(0).temperature));
+//		weather.setText(forecast.get(0).shortForecast);
+//		if (pointData == null){
+//			throw new RuntimeException("Point Data did not load");
+//		}
+//		gridX = new TextField();
+//		gridY = new TextField();
+//		gridX.setText(String.valueOf(pointData.gridX));
+//		gridY.setText(String.valueOf(pointData.gridY));
+
+		FXMLLoader dashboardLoader = new FXMLLoader(getClass().getResource("/FXML/dashboard.fxml"));
+		Parent dashboardRoot = dashboardLoader.load();
+
+		Scene dashboardScene = new Scene(dashboardRoot, 700,700);
 		
 		
 		
 				
-		Scene scene = new Scene(new VBox(temperature,weather,gridX,gridY), 700,700);
-		primaryStage.setScene(scene);
+//		Scene scene = new Scene(new VBox(temperature,weather,gridX,gridY), 700,700);
+		primaryStage.setScene(dashboardScene);
 		primaryStage.show();
 	}
 
