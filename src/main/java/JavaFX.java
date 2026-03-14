fb5df1a5d94c15e54a695160bc881de3efa5a21b
AlgoriThai07 <thaiviet0703@gmail.com>
2026-03-13 19:58:01 -0500

    Added new class for locations

--- src/main/java/JavaFX.java
+++ src/main/java/JavaFX.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
 public class JavaFX extends Application {
 	TextField temperature,weather;
 	TextField gridX,gridY;
+	TextField location;
+	String locationName;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -25,33 +27,39 @@ public class JavaFX extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 //		primaryStage.setTitle("I'm a professional Weather App!");
-//		ArrayList<HourlyPeriod> forecast = MyWeatherAPI.getHourlyForecast("LOT",77,70);
-//		PointData pointData = MyWeatherAPI.getPointData(47.62350, -122.33290);
-//		if (forecast == null){
-//			throw new RuntimeException("Forecast did not load");
-//		}
-//		temperature = new TextField();
-//		weather = new TextField();
-//		temperature.setText("Today's weather is: "+String.valueOf(forecast.get(0).temperature));
-//		weather.setText(forecast.get(0).shortForecast);
-//		if (pointData == null){
-//			throw new RuntimeException("Point Data did not load");
-//		}
-//		gridX = new TextField();
-//		gridY = new TextField();
-//		gridX.setText(String.valueOf(pointData.gridX));
-//		gridY.setText(String.valueOf(pointData.gridY));
+		ArrayList<HourlyPeriod> forecast = MyWeatherAPI.getHourlyForecast("LOT",77,70);
+		PointData pointData = MyWeatherAPI.getPointData(47.62350, -122.33290);
+		if (forecast == null){
+			throw new RuntimeException("Forecast did not load");
+		}
+		temperature = new TextField();
+		weather = new TextField();
+		temperature.setText("Today's weather is: "+String.valueOf(forecast.get(0).temperature));
+		weather.setText(forecast.get(0).shortForecast);
+		if (pointData == null){
+			throw new RuntimeException("Point Data did not load");
+		}
+		gridX = new TextField();
+		gridY = new TextField();
+		gridX.setText(String.valueOf(pointData.gridX));
+		gridY.setText(String.valueOf(pointData.gridY));
+		location = new TextField();
+		locationName = String.valueOf(pointData.relativeLocation.properties.city) + ", " + String.valueOf(pointData.relativeLocation.properties.state);
+		location.setText(locationName);
 
-		FXMLLoader dashboardLoader = new FXMLLoader(getClass().getResource("/FXML/dashboard.fxml"));
-		Parent dashboardRoot = dashboardLoader.load();
 
-		Scene dashboardScene = new Scene(dashboardRoot, 700,700);
+
+//		FXMLLoader dashboardLoader = new FXMLLoader(getClass().getResource("/FXML/dashboard.fxml"));
+//		Parent dashboardRoot = dashboardLoader.load();
+
+//		Scene dashboardScene = new Scene(dashboardRoot, 700,700);
 		
 		
 		
 				
-//		Scene scene = new Scene(new VBox(temperature,weather,gridX,gridY), 700,700);
-		primaryStage.setScene(dashboardScene);
+		Scene scene = new Scene(new VBox(temperature,weather,gridX,gridY,location), 700,700);
+//		primaryStage.setScene(dashboardScene);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
